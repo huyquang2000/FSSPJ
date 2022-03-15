e5f58f7665bf5559b57c43bfcd3acbd6332bea0a
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2022-03-15 14:12:50 +0700

    headerupdate

--- baovietpj/src/components/Header.js
+++ baovietpj/src/components/Header.js
@@ -2,28 +2,19 @@ import React from 'react'
 import logo_header from '../img/logo_header.png'
 import { Link } from 'react-router-dom';
 import { CgProfile } from 'react-icons/cg';
-
 function Header() {
-    return ( <
-        div >
-        <
-        header >
-        <
-        div className = 'header__honme ' >
-        <
-        img className = 'logo_header'
-        src = { logo_header }
-        /> <
-        div className = 'log-in' >
-        <
-        Link to = "/" > < button className = 'log-in-btn' > Đăng nhập < /button></Link >
-        <
-        /div>
-
-        <
-        /div> < /
-        header > <
-        /div>
+    return (
+        <div>
+            <header>
+                <div className='header__honme '>
+                    <img className='logo_header' src={logo_header} />
+                    <div className='log-in'>
+                        <Link to="/"><button className='log-in-btn'>Đăng nhập</button></Link>
+                    </div>
+                    
+                </div>
+            </header>
+        </div>
     )
 }
 
